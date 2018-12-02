52d83cbf6a4f1ae5416057ad06bbca15db894c7c
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2018-12-02 20:49:32 +0300

    add templates list

--- app/SQirreL/gui/mainwindow.cpp
+++ app/SQirreL/gui/mainwindow.cpp
@@ -6,15 +6,19 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->mainToolBar->hide();
 
-    //Router& router = Router::getInstance();
-    //ui->label->setText(router.getRepository()->helloString());
+    auto toolbar = ui->mainToolBar;
+    toolbar->addAction(ui->actionTemplates);
+
+    ui->templatesDockWidget->hide();
 
     // установка оформления statusBar
     ui->statusBar->setStyleSheet("background-color: #333; color: #33bb33");
     ui->statusBar->setFont(QFont("Consolas", 14));
     ui->statusBar->showMessage(tr("State: ready 0123456789"));
+
+    //Router& router = Router::getInstance();
+    //ui->label->setText(router.getRepository()->helloString());
 
     this->showMaximized();
 }
@@ -22,4 +26,16 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionTemplates_triggered()
+{
+    if(ui->templatesDockWidget->isVisible())
+    {
+        ui->templatesDockWidget->hide();
+    }
+    else
+    {
+        ui->templatesDockWidget->show();
+    }
 }
--- app/SQirreL/gui/mainwindow.h
+++ app/SQirreL/gui/mainwindow.h
@@ -17,6 +17,9 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionTemplates_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
